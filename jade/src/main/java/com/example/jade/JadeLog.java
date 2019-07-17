cde7d0d6c2a25a517f42486575e310e6dbe5cb52
jade <jadehh@live.com>
2019-07-17 14:21:32 +0800

    新增打印数组功能
    Signed-off-by: jade <jadehh@live.com>

--- jade/src/main/java/com/example/jade/JadeLog.java
+++ jade/src/main/java/com/example/jade/JadeLog.java
@@ -11,54 +11,63 @@ import android.util.Log;
 public class JadeLog {
     private static boolean DEBUG = true;
 
-    public static void e(Object object,String message){
-        if (DEBUG){
-            if (object instanceof String){
-                Log.e(object.toString(),"----------------->" + message+"<-----------------");
-            }
-            else{
-                String []A = object.getClass().getName().split("\\.");
-                String TAG = A[A.length-1];
-                Log.e(TAG, "----------------->" + message+"<-----------------");
-            }
-        }
-    }
-    public static void v(Object object,String message){
-        if (DEBUG){
-            if (object instanceof String){
-                Log.v(object.toString(), "----------------->" + message+"<-----------------");
-            }
-            else{
-                String []A = object.getClass().getName().split("\\.");
-                String TAG = A[A.length-1];
-                Log.v(TAG, "----------------->" + message+"<-----------------");
+    public static void e(Object object, String message) {
+        if (DEBUG) {
+            if (object instanceof String) {
+                Log.e(object.toString(), "----------------->" + message + "<-----------------");
+            } else {
+                String[] A = object.getClass().getName().split("\\.");
+                String TAG = A[A.length - 1];
+                Log.e(TAG, "----------------->" + message + "<-----------------");
             }
         }
     }
 
-    public static void i(Object object,String message){
-        if (DEBUG){
-            if (object instanceof String){
-                Log.i(object.toString(), "----------------->" + message+"<-----------------");
-            }else{
-                String []A = object.getClass().getName().split("\\.");
-                String TAG = A[A.length-1];
-                Log.i(TAG, "----------------->" + message+"<-----------------");
+    public static void v(Object object, String message) {
+        if (DEBUG) {
+            if (object instanceof String) {
+                Log.v(object.toString(), "----------------->" + message + "<-----------------");
+            } else {
+                String[] A = object.getClass().getName().split("\\.");
+                String TAG = A[A.length - 1];
+                Log.v(TAG, "----------------->" + message + "<-----------------");
+            }
+        }
+    }
+
+    public static void i(Object object, String message) {
+        if (DEBUG) {
+            if (object instanceof String) {
+                Log.i(object.toString(), "----------------->" + message + "<-----------------");
+            } else {
+                String[] A = object.getClass().getName().split("\\.");
+                String TAG = A[A.length - 1];
+                Log.i(TAG, "----------------->" + message + "<-----------------");
             }
 
         }
     }
 
-    public static void w(Object object,Exception message){
-        if (DEBUG){
-            if (object instanceof String){
-                Log.w(object.toString(), "----------------->" + message+"<-----------------");
-            }else{
-                String []A = object.getClass().getName().split("\\.");
-                String TAG = A[A.length-1];
-                Log.w(TAG, "----------------->" + message+"<-----------------");
+    public static void w(Object object, Exception message) {
+        if (DEBUG) {
+            if (object instanceof String) {
+                Log.w(object.toString(), "----------------->" + message + "<-----------------");
+            } else {
+                String[] A = object.getClass().getName().split("\\.");
+                String TAG = A[A.length - 1];
+                Log.w(TAG, "----------------->" + message + "<-----------------");
             }
 
+        }
+    }
+
+    public static void printArray(double[] arr) {
+        String message = "";
+        for (int i = 0; i < arr.length; i++) {
+            message = message + arr[i] + ",";
+        }
+        if (DEBUG) {
+            System.out.println(message);
         }
     }
 }
